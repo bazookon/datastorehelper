27a0bd73cef37f3924f049ae4329d000a92b7d8a
bazookon <bazookon@gmail.com>
2020-03-10 15:04:00 -0300

    build

---
+++ build/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var datastoreHelper_1 = require("./datastoreHelper");
+module.exports = datastoreHelper_1.DatastoreHelper;
