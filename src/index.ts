cf1e892d501d024a1932e3a4e3db7abea49ef92a
bazookon <bazookon@gmail.com>
2020-03-10 15:46:08 -0300

    v1.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { DatastoreHelper } from "./datastoreHelper";
+export { DatastoreHelper , DatastoreInterface, InsertResult} from "./datastoreHelper";
 export { DatastoreService } from './datastore.service';
 export { AesEncryptDecrypt } from './aes.service';
