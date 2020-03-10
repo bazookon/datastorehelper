d794ac76ba8b0c6bfa644e16ad4efdade33dd78e
bazookon <bazookon@gmail.com>
2020-03-10 15:08:11 -0300

    gitignore

--- build/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var datastoreHelper_1 = require("./datastoreHelper");
-module.exports = datastoreHelper_1.DatastoreHelper;
