8fa42f7da4bfb99e4f06ab76fe13815ef5ff0c84
bazookon <bazookon@gmail.com>
2020-03-12 17:25:38 -0300

    Modo Appengine Standar

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import fs from 'fs';
-
 export { DatastoreHelper, DatastoreInterface, InsertResult } from "./datastoreHelper";
 export { DatastoreService } from './datastore.service';
 export { AesEncryptDecrypt } from './aes.service';
@@ -8,23 +6,4 @@ export interface prefDatasotore {
   key: string;
   iv: string;
   projectId: string;
-}
-export function init({ key, iv, projectId }: { key: string, iv: string, projectId: string }) {
-  let pref: any = {
-    key: key,
-    iv: iv,
-    projectId: projectId
-  };
-
-  let result: boolean = !Object.keys(pref).some((key) => {
-    return !pref[key];
-  });
-
-  if (result)
-    fs.writeFile('datastore.json', JSON.stringify(pref), (err) => {
-      if (err) throw err;
-      console.log('Data written to file');
-    });
-
-  return result;
 }
