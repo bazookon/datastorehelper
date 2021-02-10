d6f2bb7422acb69276bbdad63ed5f1f482bbe65c
bazookon <bazookon@gmail.com>
2021-02-10 11:43:16 -0300

    update dependencies

--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -1,14 +1,9 @@
-import { init, DatastoreService } from '../index';
+import { DatastoreService } from '../index';
 import { expect } from 'chai';
 import 'mocha';
 
 
-describe('Init Library', () => {
-  it('Should return true', () => {
-    const result = init({ key: 'sdfdffds', iv: 'ghj56434', projectId: 'SuperProject' });
-    expect(result).to.true;
-  });
-});
+
 
 
 describe('Read Library',()=>{
